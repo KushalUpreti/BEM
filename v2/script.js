ae06da9da3de73c91efad655a6a86692dd8f69dd
KushalUpreti <55836966+KushalUpreti@users.noreply.github.com>
2022-05-16 19:08:51 +0545

    updated layout

--- v2/script.js
+++ v2/script.js
@@ -1,9 +1,11 @@
 function assignListeners() {
-  const hamburger = document.querySelector(".hamburger-menu");
+  const hamburger = document.querySelector(".hamburger");
   const sidebar = document.querySelector(".sidebar");
+  const hamburgerWrapper = document.querySelector(".hamburger-wrapper");
 
   hamburger.addEventListener("click", function () {
     sidebar.classList.toggle("sidebar--expand");
+    hamburgerWrapper.classList.toggle("hamburger-wrapper--expand");
   });
 }
 
