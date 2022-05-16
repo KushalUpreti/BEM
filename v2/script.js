371e73822daadf502530de69143879c126770ba1
KushalUpreti <55836966+KushalUpreti@users.noreply.github.com>
2022-05-16 19:21:57 +0545

    fixed hamburger css

--- v2/script.js
+++ v2/script.js
@@ -2,10 +2,18 @@ function assignListeners() {
   const hamburger = document.querySelector(".hamburger");
   const sidebar = document.querySelector(".sidebar");
   const hamburgerWrapper = document.querySelector(".hamburger-wrapper");
+  const backdrop = document.querySelector(".backdrop");
 
   hamburger.addEventListener("click", function () {
     sidebar.classList.toggle("sidebar--expand");
     hamburgerWrapper.classList.toggle("hamburger-wrapper--expand");
+    backdrop.classList.toggle("backdrop--visible");
+  });
+
+  backdrop.addEventListener("click", function () {
+    sidebar.classList.toggle("sidebar--expand");
+    hamburgerWrapper.classList.toggle("hamburger-wrapper--expand");
+    this.classList.toggle("backdrop--visible");
   });
 }
 
